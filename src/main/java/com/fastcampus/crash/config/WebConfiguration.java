8fbd45714af571c1c7da124eefaf615f7df8661d
dev-jayce <dev.jayce.kr@gmail.com>
2024-03-11 14:40:08 +0900

    Part 2. 회원가입 및 로그인 API 개발

--- src/main/java/com/fastcampus/crash/config/WebConfiguration.java
+++ src/main/java/com/fastcampus/crash/config/WebConfiguration.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -35,7 +36,13 @@ public class WebConfiguration {
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors(Customizer.withDefaults())
-        .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+        .authorizeHttpRequests(
+            (requests) ->
+                requests
+                    .requestMatchers(HttpMethod.POST, "/api/*/users", "/api/*/users/authenticate")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
         .sessionManagement(
             (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .csrf(CsrfConfigurer::disable)
