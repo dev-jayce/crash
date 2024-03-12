5d1994ad90f194a419cf0a7f7b6e82f69d23a342
dev-jayce <dev.jayce.kr@gmail.com>
2024-03-12 21:26:20 +0900

    Part 4. API 실습 및 API 연동 시 주의할 점

--- src/main/java/com/fastcampus/crash/config/ApplicationConfiguration.java
+++ src/main/java/com/fastcampus/crash/config/ApplicationConfiguration.java
@@ -1,25 +1,36 @@
 package com.fastcampus.crash.config;
 
+import com.fastcampus.crash.model.coinbase.PriceResponse;
 import com.fastcampus.crash.model.crashsession.CrashSessionCategory;
 import com.fastcampus.crash.model.crashsession.CrashSessionPostRequestBody;
+import com.fastcampus.crash.model.exchange.ExchangeResponse;
 import com.fastcampus.crash.model.sessionspeaker.SessionSpeaker;
 import com.fastcampus.crash.model.sessionspeaker.SessionSpeakerPostRequestBody;
 import com.fastcampus.crash.model.user.UserSignUpRequestBody;
 import com.fastcampus.crash.service.CrashSessionService;
 import com.fastcampus.crash.service.SessionSpeakerService;
 import com.fastcampus.crash.service.UserService;
+import java.nio.charset.StandardCharsets;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 import net.datafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ApplicationConfiguration {
+
+  private static final RestClient restClient = RestClient.create();
+  private static final Logger logger = LoggerFactory.getLogger(ApplicationConfiguration.class);
 
   private static final Faker faker = new Faker();
 
@@ -34,10 +45,61 @@ public class ApplicationConfiguration {
     return new ApplicationRunner() {
       @Override
       public void run(ApplicationArguments args) throws Exception {
-        createTestUsers();
-        createTestSessionSpeakers(10);
+        //        createTestUsers();
+        //        createTestSessionSpeakers(10);
+        // TODO: Bitcoin USD 가격 조회
+        var bitcoinUsdPrice = getBitcoinUsdPrice();
+        // TODO: USD to KRW 환율 조회
+        var usdToKrwExchangeRate = getUsdToKrwExchangeRate();
+        // TODO: Bitcoin KRW 가격 계산
+        var koreanPremium = 1.1;
+        var bitcoinKrwPrice = bitcoinUsdPrice * usdToKrwExchangeRate * koreanPremium;
+
+        logger.info(String.format("BTC KRW: %.2f", bitcoinKrwPrice));
       }
     };
+  }
+
+  private Double getBitcoinUsdPrice() {
+    var response =
+        restClient
+            .get()
+            .uri("https://api.coinbase.com/v2/prices/BTC-USD/buy")
+            .retrieve()
+            .onStatus(
+                HttpStatusCode::is4xxClientError,
+                (req, res) -> {
+                  logger.error(new String(res.getBody().readAllBytes(), StandardCharsets.UTF_8));
+                })
+            .body(PriceResponse.class);
+
+    assert response != null;
+    return Double.parseDouble(response.data().amount());
+  }
+
+  private Double getUsdToKrwExchangeRate() {
+    var response =
+        restClient
+            .get()
+            .uri(
+                "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=rEo29Gjp9DQJAiEHdOXVTijxnKqRg0YH&searchdate=20240312&data=AP01")
+            .retrieve()
+            .onStatus(
+                HttpStatusCode::is4xxClientError,
+                (req, res) -> {
+                  logger.error(new String(res.getBody().readAllBytes(), StandardCharsets.UTF_8));
+                })
+            .body(ExchangeResponse[].class);
+
+    assert response != null;
+
+    var usdToKrwExchangeRate =
+        Arrays.stream(response)
+            .filter(exchangeResponse -> exchangeResponse.cur_unit().equals("USD"))
+            .findFirst()
+            .orElseThrow();
+
+    return Double.parseDouble(usdToKrwExchangeRate.deal_bas_r().replace(",", ""));
   }
 
   private void createTestUsers() {
